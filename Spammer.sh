9d993d8c6c13c4585c42037cf7c7a962665bd00e
Probolinggo Cyber Security <59297302+RyuXploit@users.noreply.github.com>
2020-01-31 20:03:16 +0700

    Add files via upload

---
+++ Spammer.sh
@@ -0,0 +1,112 @@
+#Recode.??Gak akan jadiin lu programer slurr😎😎
+#RyuXploit Wass Here and Gans forever
+#Thanks For Probolinggo Cyber Security & Probolinggo Programer Newbie
+
+clear
+figlet RYUXPLOIT77 | lolcat
+echo " #################################
+
+[+] Author    : RyuXploit AKA JayaKusuma
+[+] Youtube   : Script Kiddies
+[+] Ig        : jayakusuma404
+[+] Blog      : https://www.ryuxploit.blogspot.com
+[+] Status    : Single
+[+] No Wa     : 085859400250
+
+################################ " | lolcat
+
+sleep 2
+
+figlet TOOLS SPAMMER | lolcat
+echo "KUMPULAN TOOLS SPAM 2020 by RyuXploit" | lolcat
+echo "Untuk Cara menggunakannya cukup ketik angka tools dari listnya" | lolcat
+echo "Agar work Wajib Subscribe Channel Script Kiddies" | lolcat
+
+echo "SILAHKAN DIPILIH TOOLSNYA :" | lolcat
+echo "[1] SpamSms     [Work]
+[2] SpamWA      [Work]
+[3] SpamCall    [Work]
+[4] SpamEmai    [Work]
+[5] keluar" | lolcat
+echo [+] "Pilih Nomer :"| lolcat
+read nomor
+
+if [ $nomor = 1 ] || [ $nomor = 01 ]
+then
+sleep1
+xdg-open https://youtube.com/c/ScriptKiddies
+
+clear
+apt upgrade && apt update
+apt install git
+git clone https://github.com/Amriez/gcospam
+cd gcospam
+sh install.sh
+sh gco.sh
+fi
+
+if [ $nomor = 2 ] || [ $nomor = 02 ]
+then
+sleep1
+xdg-open https://youtube.com/c/ScriptKiddies
+
+clear
+figlet SpamWA | lolcat
+
+sleep 3
+
+clear
+pkg update -y
+pkg upgrade -y
+pkg instal python -y
+pkg instll php -y
+pkg install git -y
+git clone https://github.com/kumpulanremaja/spam-wa.git
+cd spam-wa
+php wa.php
+fi
+
+if [ $nomor = 3 ] || [ $nomor = 03 ]
+then
+slepp 1
+xdg-open https://youtube.com/c/ScriptKiddies
+
+clear
+figlet SpamCall | lolcat
+
+sleep 3
+
+clear
+$apt update && apt upgrade
+$pkg install php
+$pkg install git
+$git clone https://github.com/storiku/Call
+$cd Call
+$php spamcall.php
+fi
+
+if [ $nomor = 4 ] || [$nomor = 04 ]
+then
+slepp 1
+xdg-open https://youtube.com/c/ScriptKiddies
+
+clear
+figlet SpamEmail | lolcat
+
+slepp 3
+
+pkg install git
+pkg install php
+git clone https://github.com/revan-ar/mail-spammer
+cd mail-spammer
+php mail.php
+fi
+
+if [ $nomor = 5 ] || [$nomor = 05 ]
+then
+clear
+figlet Thanks for using | lolcat
+echo "Terima kasih sudah menggunakan tools ini" | lolcat
+echo "Jangan Lupa Subscribe Channel Script Kiddies" | lolcat
+echo "See you again😎" | lolcat
+fi
